40c86dcc094378ea145735833eaaaf3c757cf64e
caldrete <cesaraaldrete@gmail.com>
2021-09-11 13:18:03 -0700

    routes simplified. logic in different file

--- server/main/routes.js
+++ server/main/routes.js
@@ -1,53 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const qry = require('./db');
+const user_ctrl = require('./controllers/user_controller');
+const user_qry = require('./models/db_user')
+const invoice_qry = require('./models/db_invoice')
 
-router.get('/api/get/allcontacts', (req, res) => {
-	qry.getAllContacts()
-		.then(q_res => res.json(q_res.rows))
-		.catch(err => console.error(err.stack))
+router.get('/api/get/allcontacts', async (req, res) => {
+	const result = await user_ctrl.getAllContacts()
+						.then(res => {return res})
+						.catch(e => console.log(e))
+	res.json(result)
 })
 
 router.post('/api/post/updatecontact', (req, res) => {
-	const u_values = [req.body.ukey, req.body.name,
-					 req.body.email, req.body.phone]
-	
-	const a_values = [req.body.akey, req.body.street,
-					 req.body.city, req.body.state,
-					 req.body.zip]
-	
-	qry.updateContact(u_values)
-		.then(
-			qry.updateAddress(a_values)
-				.then(q_res => console.log(q_res))
-				.catch(err => console.log(err.stack))
-		)
-		.catch(q_err => console.log(q_err.stack))
-		
+	user_ctrl.updateContact(req.body)
+		.then(res => console.log(res))
+		.catch(e => console.log(e))
 })
 
 router.delete('/api/delete/contact', (req, res) => {
-	const key = req.query.key
-
-	qry.deleteAddr(key)
-		.then(
-			qry.deleteUser(key)
-				.then(q_res => console.log(q_res))
-				.catch(e => console.log(e.stack))
-		)
+	user_ctrl.deleteContact(req.query.key)
+		.then(res => console.log(res))
 		.catch(err => console.log(err.stack))	
 })
 
 router.post('/api/post/newcontact', (req, res) => {
 	const u_values = [req.body.name, req.body.email, req.body.phone]
 
-	qry.newContact(u_values)
+	user_qry.newContact(u_values)
 		.then(res => { return res.rows[0].user_id })
 		.then(key => {
 			const a_values = [key, req.body.street, req.body.city, 
 							 req.body.state, req.body.zipcode]
 							 	
-			qry.newAddr(a_values)
+			user_qry.newAddress(a_values)
 				 .catch(e => console.log(e.stack))
 		})
 		.catch(err => console.log(err.stack))	
@@ -57,18 +42,18 @@ router.post('/api/post/invoice', (req, res) => {
 	const invoice_values = [req.body.user_id, req.body.unit,
 							req.body.po, req.body.labor, 1000.00]
 
-	qry.createInvoice(invoice_values)
+	inoice_qry.createInvoice(invoice_values)
 		.then(res => { return res.rows[0].invoice_id })
 		.then(key => {
 			req.body.details.map(e => {
 				const detail_values = [key, e.desc, e.rate, e.qty]		
-				qry.postDetails(detail_values)
+				invoice_qry.postDetails(detail_values)
 					.catch(e => console.error(e.stack))
 			})
 
 			req.body.materials.map(e => {
 				const material_values = [key, e.item, e.cost, e.count]
-				qry.postMaterials(material_values)
+				invoice_qry.postMaterials(material_values)
 					.catch(e => console.error(e.stack))				
 				})
 		})
