86238bd142883dc6ec2be4fa507a08e56211dc22
caldrete <cesaraaldrete@gmail.com>
2022-01-27 14:50:42 -0800

    bcrypt added to dependencies and controller for registration

--- server/main/controllers/auth_controller.js
+++ server/main/controllers/auth_controller.js
@@ -1,12 +1,16 @@
 const auth = require('../models/db_auth')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
-const register = async body => {
-	const values = [body.user, body.pwd]
-	const result = await auth.register(values)
-		.then(q_res => console.log(q_res))
-		.catch(err => console.log(err.stack))
+const register = body => {
 
-	console.log('Controller Done')
+	bcrypt.hash(body.pwd, saltRounds, async (err, hash) => {
+		const values = [body.user, hash]
+		console.log(hash)
+		const result = await auth.register(values)
+			.then(q_res => console.log(q_res))
+			.catch(err => console.log(err.stack))
+	})
 }
 
 module.exports = {
